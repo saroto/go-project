0f50973fcf9d8de1c3eed1fed1c2a00b54b1a2d5
kimsinh_DM <kimsinh.seang@ams3e.com>
2025-09-10 16:16:47 +0700

    first commit

--- config/database.go
+++ config/database.go
@@ -2,14 +2,20 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func DbConnection() {
+func DbConnection() *gorm.DB {
 	sqlDB, err := sql.Open("pgx", "mydb_dsn")
+	if err != nil {
+		fmt.Printf("Error database connection: ", err)
+	}
 	gormDB, err := gorm.Open(postgres.New(postgres.Config{
 		Conn: sqlDB,
 	}), &gorm.Config{})
+
+	return gormDB
 }
